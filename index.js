ea82be8badc75d12656a1d79cd661b5c17917c3d
TheSubliminal <fridrihson.1701@gmail.com>
2019-03-10 15:54:22 +0200

    Proxy server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const http = require('http');
+//const http = require('http');
 //const https = require('https');
 //const fs = require('fs');
 const url = require('url');
@@ -8,13 +8,13 @@ const url = require('url');
 //let content = fs.readFileSync('index.html');
 
 
-module.exports = (request, response) => {
+require('http').createServer((request, response) => {
 
     // console.log(request.url);
     console.log(request.url, url.parse(request.url).protocol);
 
     //if (request.url.split(':')[0] === 'http') {
-        http.get(request.url, (resp) => {
+    require('http').get(request.url, (resp) => {
 
             resp.pipe(response);
 
@@ -33,4 +33,4 @@ module.exports = (request, response) => {
         });
     }*/
 
-};
+}).listen(8000);
