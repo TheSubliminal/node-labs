1c46edaf79a8d59bfff654222aac8cc69d9db54b
TheSubliminal <fridrihson.1701@gmail.com>
2019-03-10 16:07:24 +0200

    Proxy server

--- index.js
+++ index.js
@@ -11,26 +11,16 @@ const url = require('url');
 require('http').createServer((request, response) => {
 
     // console.log(request.url);
-    console.log(request.url, url.parse(request.url).protocol);
+    console.log('URLS: ', request.url, url.parse(request.url).protocol);
 
-    //if (request.url.split(':')[0] === 'http') {
-    require('http').get(request.url, (resp) => {
+    if (request.url) {
+        require('http').get(request.url, (resp) => {
 
             resp.pipe(response);
 
         }).on('error', err => {
-            console.log('Error: ', err.message);
+            console.log('Error from get: ', err.message);
         });
-    //}
-
-    /*if (request.url.split(':')[0] === 'https') {
-        https.get('https://' + request.url.split('://')[1], resp => {
-            console.log('https://' + request.url.split('://')[1]);
-            resp.pipe(response);
-
-        }).on('error', err => {
-            console.log('Error: ', err.message);
-        });
-    }*/
+    }
 
 }).listen(8000);
