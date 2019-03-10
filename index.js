74f0118b022bb49ff0a9868b53238a5b52127bd7
TheSubliminal <fridrihson.1701@gmail.com>
2019-03-10 14:39:56 +0200

    Proxy server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const http = require('http');
+//const http = require('http');
 //const https = require('https');
 //const fs = require('fs');
 const url = require('url');
@@ -8,40 +8,32 @@ const url = require('url');
 //let content = fs.readFileSync('index.html');
 
 
-const startServer = () => {
+module.exports = (request, response) => {
 
-    const server = http.createServer((request, response) => {
+    // console.log(request.url);
+    console.log(request.url, url.parse(request.url).protocol);
 
-        // console.log(request.url);
-        console.log(request.url, url.parse(request.url).protocol);
-
-        response.end('Hello world!');
+    response.end('Hello world!');
 
 
-        //if (request.url.split(':')[0] === 'http') {
-            /*http.get(request.url, (resp) => {
+    //if (request.url.split(':')[0] === 'http') {
+        /*http.get(request.url, (resp) => {
 
-                resp.pipe(response);
-    
-            }).on('error', err => {
-                console.log('Error: ', err.message);
-            });*/
-        //}
+            resp.pipe(response);
 
-        /*if (request.url.split(':')[0] === 'https') {
-            https.get('https://' + request.url.split('://')[1], resp => {
-                console.log('https://' + request.url.split('://')[1]);
-                resp.pipe(response);
-    
-            }).on('error', err => {
-                console.log('Error: ', err.message);
-            });
-        }*/
+        }).on('error', err => {
+            console.log('Error: ', err.message);
+        });*/
+    //}
 
-    });
+    /*if (request.url.split(':')[0] === 'https') {
+        https.get('https://' + request.url.split('://')[1], resp => {
+            console.log('https://' + request.url.split('://')[1]);
+            resp.pipe(response);
 
-    server.listen();
+        }).on('error', err => {
+            console.log('Error: ', err.message);
+        });
+    }*/
 
 };
-
-startServer();
