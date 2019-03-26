09922bdad17caa825837473de2eb3a67904ac173
TheSubliminal <fridrihson.1701@gmail.com>
2019-03-26 23:36:32 +0200

    Sockets upgrade event

--- index.js
+++ index.js
@@ -1,43 +1,72 @@
-'use strict';
+'use strict'
 
+const https = require('https');
 const http = require('http');
-const net = require('net');
 const url = require('url');
+const WebSocket = require('ws');
 
-const proxy = (request, response) => {
+//const proxy = (request, response) => {
+const proxy = http.createServer((request, response) => {
 
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    if (request.url === '/')
-    {
+    //response.setHeader('Content-Type', 'text/javascript');
+
+    let reqUrl = url.parse(request.url);
+    //console.dir(reqUrl);
+    if (request.url === '/') {
         console.log('REQUEST TO SERVER\'S PAGE', new Date().toLocaleTimeString());
         response.end('This is server\'s page');
-    }
-    else if (url.parse(request.url).protocol === 'http:') {
+    } else if (reqUrl.protocol === 'http:') {
         console.log('HTTP GET:', request.url, new Date().toLocaleTimeString());
         http.get(request.url, (resp) => {
             resp.pipe(response);
         }).on('error', err => {
-            console.log('ERROR GET: ', err.message);
+            console.log('HTTP ERROR: ', err.message);
+        });
+    } else if (reqUrl.protocol === 'https:') {
+        console.log('HTTPS GET:', request.url, new Date().toLocaleTimeString());
+        https.get(request.url, (resp) => {
+            resp.pipe(response);
+        }).on('error', err => {
+            console.log('HTTPS ERROR: ', err.message);
         });
     }
+});
+//};
 
-};
+proxy.on('upgrade', (request, cltSocket) => {
+    console.log('UPGRADE EVENT: ', request.url, new Date().toLocaleTimeString());
 
-proxy.on('connect', (req, cltSocket) => {
-    console.log('HTTPS GET: ', req.url, new Date().toLocaleTimeString());
-    const srvUrl = url.parse(`http://${req.url}`);
-    const srvSocket = net.connect(srvUrl.port, srvUrl.hostname, () => {
-        cltSocket.write('HTTP/' + req.httpVersion + ' 200 OK\r\n' +
-            '\r\n', 'UTF-8', () =>{
-            srvSocket.pipe(cltSocket);
-            cltSocket.pipe(srvSocket);
-        });
-
-    }).on('error', (err) => {
-        console.log('ERROR SOCKET: ', err);
-    });
+}).on('error', (err) => {
+    console.log('ERROR UPGRADE: ', err);
 });
 
+proxy.on('connect', (request, cltSocket) => {
+   console.log('CONNECT', request.url);
+   if (!request.url.includes('443')) {
+       const ws = new WebSocket('ws://echo.websocket.org/', {
+           origin: 'http://websocket.org'
+       });
+
+       ws.on('open', function open() {
+           console.log('connected');
+           cltSocket.pipe(ws);
+           ws.send(Date.now());
+       });
+
+       ws.on('close', function close() {
+           console.log('disconnected');
+       });
+
+       ws.on('message', function incoming(data) {
+           console.log(`Roundtrip time: ${Date.now() - data} ms`);
+
+           setTimeout(function timeout() {
+               ws.send(Date.now());
+           }, 500);
+       });
+
+   }
+});
 
 const listener = proxy.listen(8000, (err) => {
     const SERVER_IP = listener.address().address;
