e6da1162e3de66209dae5bb651d3720d799cbaa8
TheSubliminal <fridrihson.1701@gmail.com>
2019-03-29 09:47:37 +0200

    Socket ping via ws library

--- index.js
+++ index.js
@@ -1,80 +1,29 @@
 'use strict'
 
-const https = require('https');
-const http = require('http');
-const url = require('url');
 const WebSocket = require('ws');
 
-//const proxy = (request, response) => {
-const proxy = http.createServer((request, response) => {
+const webSock = new WebSocket('ws://echo.websocket.org');
 
-    //response.setHeader('Content-Type', 'text/javascript');
+let timer;
 
-    let reqUrl = url.parse(request.url);
-    //console.dir(reqUrl);
-    if (request.url === '/') {
-        console.log('REQUEST TO SERVER\'S PAGE', new Date().toLocaleTimeString());
-        response.end('This is server\'s page');
-    } else if (reqUrl.protocol === 'http:') {
-        console.log('HTTP GET:', request.url, new Date().toLocaleTimeString());
-        http.get(request.url, (resp) => {
-            resp.pipe(response);
-        }).on('error', err => {
-            console.log('HTTP ERROR: ', err.message);
-        });
-    } else if (reqUrl.protocol === 'https:') {
-        console.log('HTTPS GET:', request.url, new Date().toLocaleTimeString());
-        https.get(request.url, (resp) => {
-            resp.pipe(response);
-        }).on('error', err => {
-            console.log('HTTPS ERROR: ', err.message);
-        });
-    }
-});
-//};
-
-proxy.on('upgrade', (request, cltSocket) => {
-    console.log('UPGRADE EVENT: ', request.url, new Date().toLocaleTimeString());
-
-}).on('error', (err) => {
-    console.log('ERROR UPGRADE: ', err);
+webSock.on('connection', (socket, request) => {
+    console.log('connection', socket.address());
 });
 
-proxy.on('connect', (request, cltSocket) => {
-   console.log('CONNECT', request.url);
-   if (!request.url.includes('443')) {
-       const ws = new WebSocket('ws://echo.websocket.org/', {
-           origin: 'http://websocket.org'
-       });
-
-       ws.on('open', function open() {
-           console.log('connected');
-           cltSocket.pipe(ws);
-           ws.send(Date.now());
-       });
-
-       ws.on('close', function close() {
-           console.log('disconnected');
-       });
-
-       ws.on('message', function incoming(data) {
-           console.log(`Roundtrip time: ${Date.now() - data} ms`);
-
-           setTimeout(function timeout() {
-               ws.send(Date.now());
-           }, 500);
-       });
-
-   }
+webSock.on('open', function open() {
+    webSock.send('something');
+    timer = setInterval(() => webSock.send('something'), 1000);
 });
 
-const listener = proxy.listen(8000, (err) => {
-    const SERVER_IP = listener.address().address;
-    const SERVER_PORT = listener.address().port;
-    console.log(`Listening on IP: ${SERVER_IP} with PORT: ${SERVER_PORT}`);
-    if (err) {
-        console.log('ERROR LISTEN: ', err);
-    }
+webSock.on('message', function incoming(data) {
+    console.log(data);
 });
 
-module.exports = proxy;
+webSock.on('upgrade', (response) => {
+    console.log('Upgrade event');
+});
+
+webSock.on('close', () => {
+    console.log('Closed');
+    clearInterval(timer);
+});
