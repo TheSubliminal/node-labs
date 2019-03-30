47522e5a430ca5752216285a5c3f308c4ef8e573
TheSubliminal <fridrihson.1701@gmail.com>
2019-03-30 16:48:57 +0200

    Completed parsinf RozkladKPI to accessable format

--- index.js
+++ index.js
@@ -4,16 +4,63 @@ const http = require('http');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const url = 'http://rozklad.kpi.ua/Schedules/ViewSchedule.aspx?g=894be0b0-9c4b-492e-a3d0-a6950cb1a3e1';
+const URL = 'http://rozklad.kpi.ua/Schedules/ViewSchedule.aspx?g=894be0b0-9c4b-492e-a3d0-a6950cb1a3e1';
 
 const parse = function parse(htmlData) {
+
+    const document = new JSDOM(htmlData).window.document;
+
+    const firstWeekTable = document.getElementById('ctl00_MainContent_FirstScheduleTable').getElementsByTagName('tr');
+    const secondWeekTable = document.getElementById('ctl00_MainContent_SecondScheduleTable').getElementsByTagName('tr');
+    const firstWeekLessons = lessonsPerWeek(firstWeekTable);
+    const secondWeekLessons = lessonsPerWeek(secondWeekTable);
+    const firstWeek = formDays(firstWeekLessons);
+    const secondWeek = formDays(secondWeekLessons);
+
+    for (let [day, schedule] of firstWeek.entries()) {
+        console.log(`\n\nSchedule for ${day}:\n-----------`);
+        for (let lesson of schedule) {
+            if (lesson.name) {
+                console.log(`Pair: ${lesson.name}\nTeacher: ${lesson.teacher}\nPlace: ${lesson.place}\n-----------`);
+            }
+            else {
+                console.log('No pair\n-----------');
+            }
+        }
+    }
+};
+
+const formDays = function formDays(lessonsByTime) {
+
+    const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednsday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    const initialWeek = [];
+    for (let idx in lessonsByTime[0]) {
+        initialWeek.push([WEEK_DAYS[idx], []]);
+    }
+    const days = new Map(initialWeek);
+
+    let scheduleForDay;
+    for (let lessonNumber of lessonsByTime) {
+        for (let nthDay in lessonNumber)
+        {
+            scheduleForDay = days.get(WEEK_DAYS[nthDay]);
+            scheduleForDay.push(lessonNumber[nthDay]);
+            //days.set(WEEK_DAYS[nthDay], scheduleForDay.push(lessonNumber[nthDay]));
+        }
+    }
+
+    return days;
+};
+
+const lessonsPerWeek = function lessonsPerWeek(weekTable) {
+
     const lessons = [];
     const pairsPerTime = [];
-    const document = new JSDOM(htmlData).window.document;
-    const first_week = document.getElementById('ctl00_MainContent_FirstScheduleTable').getElementsByTagName('tr');
-    for (let index in first_week) {
+
+    for (let index in weekTable) {
         if (index > 0 && index < 5) {
-            pairsPerTime.push(first_week[index].getElementsByTagName('td'));
+            pairsPerTime.push(weekTable[index].getElementsByTagName('td'));
         }
     }
     //console.log(pairsPerTime[0][1].getElementsByTagName('a')[0].innerHTML);
@@ -23,33 +70,32 @@ const parse = function parse(htmlData) {
             if (pair > 0 && pair < 6) {
                 let lesson = {};
                 let tags = day[pair].getElementsByTagName('a');
-                if (!tags[0]) {
+                if (tags.length === 0) {
                     lesson = {};
                 }
                 else {
                     lesson.name = tags[0].innerHTML;
-                    try {
-                        lesson.teacher = tags[1].innerHTML;
+                    let teachers = [], places = [];
+                    for (let i = 1; i < tags.length; i++) {
+                        if (tags[i].getAttribute('href').includes('/Schedules/ViewSchedule')) {
+                            teachers.push(tags[i].innerHTML);
+                        }
+                        else if (tags[i].getAttribute('href').includes('maps.google.com')) {
+                            places.push(tags[i].innerHTML);
+                        }
                     }
-                    catch (err) {
-                        lesson.teacher = '';
-                    }
-                    try {
-                        lesson.place = tags[2].innerHTML;
-                    }
-                    catch (err) {
-                        lesson.place = '';
-                    }
+                    lesson.teacher = teachers.join(', ');
+                    lesson.place = places.join(', ');
                 }
                 nthLessonOfWeek.push(lesson);
             }
         }
         lessons.push(nthLessonOfWeek);
     }
-    console.log(lessons);
+    return lessons;
 };
 
-http.get(url, (response) => {
+http.get(URL, (response) => {
 
     let data = '';
 
